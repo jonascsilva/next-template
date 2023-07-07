d8ecf5816cdd66764d2304b2a0ab078ca7ff7405
jonascsilva <55038425+jonascsilva@users.noreply.github.com>
2023-07-07 01:52:05 +0000

    chore: update next to version 13.4.9

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Next.js version 13 template
+
+```
+npx degit https://github.com/JonasCSilva/next-template.git <project-name>
+```
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import styles from './page.module.scss'
 export default function Page() {
   return (
     <main className={styles.main}>
-      <h1>Next.js 13.3.4</h1>
+      <h1>Next.js 13.4.9</h1>
     </main>
   )
 }

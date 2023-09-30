4de7acfca945d931f4e584e8b5fae08fcba84871
jonascsilva <55038425+jonascsilva@users.noreply.github.com>
2023-09-30 14:53:40 -0300

    Update to version 13.5.3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import styles from './page.module.scss'
 export default function Page() {
   return (
     <main className={styles.main}>
-      <h1>Next.js 13.4.13</h1>
+      <h1 className={styles.heading}>Next.js 13.5.3</h1>
     </main>
   )
 }

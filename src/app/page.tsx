d71542ae0901bf35fe5d803f24703f8429f2e916
jonascsilva <55038425+jonascsilva@users.noreply.github.com>
2024-01-01 00:15:05 -0300

    style index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import classes from './page.module.scss'
 
 const Page = () => (
   <main className={classes.main}>
-    <h1 className={classes.heading}>Next.js template</h1>
     <ThemeButton />
+    <h1 className={classes.heading}>Next.js Template</h1>
   </main>
 )
 
